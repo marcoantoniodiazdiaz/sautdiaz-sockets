4f01fd340333b2c8c696b41b1e02b4d7625bae42
Marco Antonio Diaz Diaz <kingmarkdc@gmail.com>
2019-08-18 15:54:04 -0500

    Solucion de conexion

--- global/environment.ts
+++ global/environment.ts
@@ -6,4 +6,5 @@ export const TOKEN_CAD = '30d';
 export const URL_DB =
   process.env.NODE_ENV === 'dev'
     ? 'mongodb+srv://marco_diaz:pataPON3@cluster0-jm5fl.mongodb.net/sautdiaz?retryWrites=true&w=majority'
-    : 'mongodb://localhost:27017/sautdiaz';
+    : // ? 'mongodb+srv://marco_diaz:pataPON3@cluster0-jm5fl.mongodb.net/sautdiaz?retryWrites=true&w=majority'
+      'mongodb://localhost:27017/sautdiaz';
